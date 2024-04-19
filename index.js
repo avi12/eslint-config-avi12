5b658d2f61f88c135b22664e7ec7c9b3fb7e6dd1
avi12 <avi6106@gmail.com>
2024-04-19 09:49:50 +0300

    Added stuff

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import tsEslint from 'typescript-eslint';
 import pluginImport from 'eslint-plugin-import';
+import stylistic from '@stylistic/eslint-plugin-js';
 
 export default [
   {
     plugins: {
       '@typescript-eslint': tsEslint.plugin,
+      '@stylistic/js': stylistic,
       import: pluginImport,
     },
     rules: {
@@ -38,6 +40,7 @@ export default [
         'warn',
         { groups: ['external', 'internal'], alphabetize: { order: 'asc' } },
       ],
+      '@stylistic/js/object-curly-spacing': ['warn', 'always'],
     },
   },
 ];
