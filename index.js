186d286c0a9dcbfc2caac4a92ee8c7a8b66f1994
Avi <avi6106@gmail.com>
2025-05-04 06:17:40 +0300

    fixed rule

--- index.js
+++ index.js
@@ -1,43 +1,41 @@
-import pluginImport from 'eslint-plugin-import';
-import stylistic from '@stylistic/eslint-plugin-js';
+import pluginImport from "eslint-plugin-import";
+import stylistic from "@stylistic/eslint-plugin-js";
 
 export default [
   {
     plugins: {
-      '@stylistic/js': stylistic,
-      import: pluginImport,
+      "@stylistic/js": stylistic,
+      import: pluginImport
     },
     rules: {
-      'arrow-parens': ['warn', 'as-needed'],
-      'prefer-const': 'warn',
-      'no-async-promise-executor': 'off',
-      semi: 'warn',
+      "arrow-parens": ["warn", "as-needed"],
+      "prefer-const": "warn",
+      "no-async-promise-executor": "off",
+      semi: "warn",
       quotes: [
-        'warn',
-        'double',
+        "warn",
+        "double",
         {
           allowTemplateLiterals: true,
-        },
+          avoidEscape: true
+        }
       ],
       indent: [
-        'warn',
+        "warn",
         2,
         {
-          SwitchCase: 1,
-        },
+          SwitchCase: 1
+        }
       ],
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': ['warn'],
-      'object-curly-spacing': ['warn', 'always'],
-      'quote-props': ['warn', 'as-needed'],
-      'comma-dangle': ['warn', 'never'],
-      curly: ['warn', 'all'],
-      'array-element-newline': ['warn', 'consistent'],
-      'import/order': [
-        'warn',
-        { groups: ['external', 'internal'], alphabetize: { order: 'asc' } },
-      ],
-      '@stylistic/js/object-curly-spacing': ['warn', 'always'],
-    },
-  },
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["warn"],
+      "object-curly-spacing": ["warn", "always"],
+      "quote-props": ["warn", "as-needed"],
+      "comma-dangle": ["warn", "never"],
+      curly: ["warn", "all"],
+      "array-element-newline": ["warn", "consistent"],
+      "import/order": ["warn", { groups: ["external", "internal"], alphabetize: { order: "asc" } }],
+      "@stylistic/js/object-curly-spacing": ["warn", "always"]
+    }
+  }
 ];
